ba575ecaaa6d8c870e04c7f4993af3191391551d
alexdeane1 <alex-9-@live.co.uk>
2022-04-12 15:01:38 +0100

    header changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
           muli: ['muli', 'sans-serif'],
           roboto: ['Roboto', 'sans-serif']
       },
+      screens:{
+        '3xl': '1650px',
+    },
       colors: {
           transparent: 'transparent',
           primary: '#005DAC',
